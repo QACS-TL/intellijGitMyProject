297704594b9b6a4470e6bfd292f44a5444f7a37f
QACS-TL <PBehague@qa.com>
2023-07-07 14:21:03 +0100

    banana with added ice cream

--- src/main/java/Banana.java
+++ src/main/java/Banana.java
@@ -2,4 +2,8 @@ public class Banana{
     public String peel(){
         return "I'm being peeled and it hurts!";
     }
+
+    public String addIceCream(){
+        return "Ice cream added!";
+    }
 }
