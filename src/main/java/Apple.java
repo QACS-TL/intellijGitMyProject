29e5a8b07180a3b2ba310e7fb3769c25710c991c
QACS-TL <PBehague@qa.com>
2023-07-07 14:16:26 +0100

    banana slip and apple added

---
+++ src/main/java/Apple.java
@@ -0,0 +1,5 @@
+public class Apple {
+    public String redOrGreen(){
+        return "Red!";
+    }
+}
--- src/main/java/Banana.java
+++ src/main/java/Banana.java
@@ -2,4 +2,8 @@ public class Banana{
     public String peel(){
         return "I'm being peeled and it hurts!";
     }
+
+    public String slip(){
+        return "I'm being slippery!";
+    }
 }
